585735ceab9056a8da13b2cf7de35882ca2fd96d
Tori Godfrey <victoria.godfrey@segment.com>
2020-06-23 21:59:22 -0600

    added repo link

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
     <div className="App">
       <h1>Weather Search Engine</h1>
       <Searchbar />
+      <a href="https://github.com/inertiawhoosh/react-weather-app">View Repository</a>
     </div>
   );
 }
