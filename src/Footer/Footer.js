3354975b0bc3ab6d182386e287bcb2c40b7fc360
Victoria Godfrey <victorialgodfrey@gmail.com>
2020-06-29 17:07:52 -0600

    fixed some styling

---
+++ src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './footer.css'
+
+
+export default function Footer() {
+  return (
+    <div id="footer">
+      <a href="https://github.com/inertiawhoosh/react-weather-app">View Repository</a>
+    </div>
+  );
+}
